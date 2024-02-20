b465ec03fd7584f12fc745e70af111db824c7854
SamPrestoo <160303378+SamPrestoo@users.noreply.github.com>
2024-02-19 23:33:53 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,10 +9,6 @@ app.use(express.static('public'))
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
 
-require('dotenv').config();
-
-console.log(process.env.DB_HOST); // 'localhost'
-
 
 const storeItems = new Map([
     [1, { priceInCents: 10000, name: "Learn React Today"}],
